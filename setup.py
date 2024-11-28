a2acf6e1f46846e356cc31c23a25cbf142fb9679
dan <katosnaprestige@gmail.com>
2024-11-28 17:11:12 +0300

    Merge remote-tracking branch 'origin/master'

--- setup.py
+++ setup.py
@@ -3,25 +3,24 @@ from setuptools import setup, find_packages
 setup(
     name="backup-script",
     version="0.1",
-    packages=find_packages(),  # Находит все папки с кодом
-    install_requires=[         # Зависимости, которые должны быть установлены
+    packages=find_packages(),
+    install_requires=[
         'psutil',
     ],
-    entry_points={             # Запуск программы через команду в терминале
+    entry_points={
         'console_scripts': [
-            'backup-script=backup_script.main:main',  # Здесь указываем точку входа
+            'lentochka=backup_script.BrandedKrait(DSMC):main',
         ],
     },
-    include_package_data=True,  # Включаем все файлы в пакете (например, config.ini)
-    package_data={             # Включаем дополнительные файлы
+    include_package_data=True,
+    package_data={
         '': ['config.ini'],
     },
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    classifiers=[             # Категории, под которые подходит проект
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-)
